bf4d024be6a1db72f62836607b0ac12445a4e754
imani-abayakoon <imani@apium.io>
2021-06-21 10:12:00 +0530

    #368

--- scripts/clickhandler.js
+++ scripts/clickhandler.js
@@ -6,14 +6,13 @@ AFRAME.registerComponent('clickhandler', {
 
     init: function() {
         console.log('In');
-
-        //console.log(this.targetElement)
-        this.targetElement = document.querySelector(this.data.modelId);
+        console.log(this.el.sceneEl);
+        this.targetElement = this.data.modelId && document.querySelector(this.data.modelId);
         console.log(this.targetElement);
         var v = document.querySelector(this.data.videoId);
         console.log(v);
-        this.targetElement.addEventListener('click', () => {
-            //console.log('In Listener')
+        this.targetElement.addEventListener("click", (e) => {
+            console.log('In Listener');
             alert('Clicked');
             v.play();
         });		
